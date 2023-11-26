066951b890570a5179fd7b55074ae78a8ed66245
ted wu <r05543056@g.ntu.edu.tw>
2023-11-26 13:10:06 +0900

    chore: replace public file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter, Space_Grotesk } from 'next/font/google'
 import './globals.css'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 const SpaceGrotesk = Space_Grotesk({ 
@@ -20,7 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className='max-w-10xl mx-auto'>
+          <Navbar />
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
